f1cbb2bfcd304c43427da77c5e097f95176a9bec
Egija Kokorevica <119627264+Egijak@users.noreply.github.com>
2023-03-05 23:20:23 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,33 +1,67 @@
 # python3
+# Egija Kokoreviča 	221RDB288
 
 import sys
 import threading
-import numpy
+
 
 
 def compute_height(n, parents):
-    # Write this function
-    max_height = 0
-    # Your code here
-    return max_height
+    children = [[] for _ in range(n)]
+    for i, parent in enumerate(parents):
+        if parent!=-1:
+            children[parent].append(i)
+
+    def get_height(node):
+        if not children[node]:
+            return 1
+        return 1+max(get_height(child)for child in children[node])
+    root=parents.index(-1)
+    return get_height(root)
+
+
+    
 
 
 def main():
-    # implement input form keyboard and from files
-    
-    # let user input file name to use, don't allow file names with letter a
-    # account for github input inprecision
-    
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
-    pass
 
-# In Python, the default limit on recursion depth is rather low,
-# so raise it here for this problem. Note that to take advantage
-# of bigger stack, we have to launch the computation in a new thread.
+    '''Printing answer'''
+
+    ievade = input("Ievdi F vai I:")
+    if "F" in ievade:
+        file = input()
+
+        if "a" in file:
+            print("You can't use file names with letter 'a'")
+            return
+        try:
+    
+            with open (file) as fp:
+                n = int(fp.readline())
+                parents = list(map(int, fp.readline().split()))
+
+        except FileNotFoundError:
+            print("Inprecision in the file name")
+            return
+       
+            
+    if "I" in ievade:  
+        try:
+                    
+            n = int(input()) 
+            parents = list(map(int, input().split()))
+
+        except ValueError:
+            print("Inprecision in input")
+            return  
+
+    print(compute_height(n, parents))
+
+
+
+
+    
 sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)   # new thread will get stack of such size
 threading.Thread(target=main).start()
-main()
-# print(numpy.array([1,2,3]))
+
